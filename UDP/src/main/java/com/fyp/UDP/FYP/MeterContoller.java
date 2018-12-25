2a3240e60f80171b012fea2d39af0907815afbf6
Buddhima Udaranga <buddhimaudaranga1994@gmail.com>
2018-12-25 06:00:44 +0530

    BuiltAdminPortalAdded

--- UDP/src/main/java/com/fyp/UDP/FYP/MeterContoller.java
+++ UDP/src/main/java/com/fyp/UDP/FYP/MeterContoller.java
@@ -5,11 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.criteria.CriteriaBuilder;
+import java.sql.Timestamp;
+import java.util.Date;
 
 /**
  * Created by Buddhima on 11/13/2018.
  */
 @Controller
+@CrossOrigin(origins = {"http://localhost:8081","*"})
 @RequestMapping(path ="/meter")
 @ResponseBody
 public class MeterContoller {
@@ -18,9 +21,12 @@ public class MeterContoller {
 
     @RequestMapping(value="/add", method = RequestMethod.POST)
    public boolean addData(@RequestBody MeterData m){
+        Date date = new Date();
+        Timestamp ts = new Timestamp(date.getTime());
        MeterData meterData = new MeterData();
        meterData.setId(m.getId());
        meterData.setData(m.getData());
+       meterData.setTime(ts);
        meterRepository.save(meterData);
        return true;
    }
--- UDP/src/main/java/com/fyp/UDP/FYP/MeterData.java
+++ UDP/src/main/java/com/fyp/UDP/FYP/MeterData.java
@@ -3,6 +3,7 @@ package com.fyp.UDP.FYP;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.sql.Timestamp;
 
 /**
  * Created by Buddhima on 11/13/2018.
@@ -13,6 +14,8 @@ public class MeterData {
     private Integer id;
 
     private String data;
+
+    private Timestamp time;
 
     public Integer getId() {
         return id;
@@ -28,5 +31,13 @@ public class MeterData {
 
     public void setData(String data) {
         this.data = data;
+    }
+
+    public Timestamp getTime() {
+        return time;
+    }
+
+    public void setTime(Timestamp time) {
+        this.time = time;
     }
 }
